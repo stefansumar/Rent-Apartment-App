e6a637be9f93c3438ee7ef1dc536972606d22f90
Stefan <39972111+stefansumar@users.noreply.github.com>
2020-08-02 18:41:18 +0200

    Validation for change password fields implemented.

--- PocetniREST/WebContent/js/changePassword.js
+++ PocetniREST/WebContent/js/changePassword.js
@@ -1,3 +1,5 @@
+var currentUser;
+
 function changePassword(){
     $.get({
         url: 'rest/user/getCurrentUser',
@@ -13,3 +15,70 @@ function changePassword(){
         }
     });
 }
+
+$(document).ready(function() {
+	$('#changePasswordForm').submit(function(event){
+		event.preventDefault();
+		let realPassword = currentUser.password;
+		let currentPassword = $('#currentPassword').val();
+		let newPassword = $('#newPassword').val();
+		let repeatNewPassword = $('#repeatNewPassword').val();
+		console.log(realPassword)
+		console.log(currentPassword);
+		console.log(newPassword);
+		console.log(repeatNewPassword);
+		
+		if(!currentPassword && !newPassword && !repeatNewPassword){
+			$('#errorPasswordMessage').text('All fields are required!');
+			$('#errorPasswordMessage').css({"color": "red", "font-size": "12px"});
+			$('#errorPasswordMessage').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		if(!currentPassword){
+			$('#emptyCurrentPassword').text('Current password is required!');
+			$('#emptyCurrentPassword').css({"color": "red", "font-size": "12px"});
+			$('#emptyCurrentPassword').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		if(currentPassword !== realPassword){
+			$('#matchCurrentPassword').text('Wrong password');
+			$('#matchCurrentPassword').css({"color": "red", "font-size": "12px"});
+			$('#matchCurrentPassword').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		if(!newPassword){
+			$('#emptyNewPassword').text('New password is required!');
+			$('#emptyNewPassword').css({"color": "red", "font-size": "12px"});
+			$('#emptyNewPassword').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		if(newPassword.length < 8){
+			$('#lengthNewPassword').text('New password must contain min. 8 characters!');
+			$('#lengthNewPassword').css({"color": "red", "font-size": "12px"});
+			$('#lengthNewPassword').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		if(!repeatNewPassword){
+			$('#emptyRepeatNewPassword').text('Reenter password is required!');
+			$('#emptyRepeatNewPassword').css({"color": "red", "font-size": "12px"});
+			$('#emptyRepeatNewPassword').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		if(repeatNewPassword !== newPassword){
+			$('#matchNewPassword').text('Passwords does not match!');
+			$('#matchNewPassword').css({"color": "red", "font-size": "12px"});
+			$('#matchNewPassword').show().delay(3000).fadeOut();
+			return;
+		}
+		
+		
+	});	
+	
+	
+});
